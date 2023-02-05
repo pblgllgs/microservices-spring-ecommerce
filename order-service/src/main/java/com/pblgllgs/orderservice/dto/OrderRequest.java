fe1e241a696230ed04562e2b7290e844c5559e62
pblgllgs <pbl.gllgs@gmail.com>
2023-02-05 16:03:31 -0300

    initial commit 3 services, order, products and inventory, mysql and mongo

---
+++ order-service/src/main/java/com/pblgllgs/orderservice/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package com.pblgllgs.orderservice.dto;
+
+import com.pblgllgs.orderservice.model.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
