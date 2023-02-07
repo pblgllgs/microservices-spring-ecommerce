732574f7df833d0741e0eb4591bbd9141b528764
pblgllgs <pbl.gllgs@gmail.com>
2023-02-07 11:40:27 -0300

    spring boot sleuth distributed tracing with zipkin UI view of traces

--- order-service/src/main/java/com/pblgllgs/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/pblgllgs/orderservice/service/OrderService.java
@@ -7,6 +7,9 @@ import com.pblgllgs.orderservice.model.OrderLineItems;
 import com.pblgllgs.orderservice.repository.OrderRepository;
 import com.pblgllgs.orderservice.dto.InventoryResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -18,11 +21,14 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
 
     private final WebClient.Builder webClientBuilder;
+
+    private final Tracer tracer;
 
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -35,20 +41,27 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream()
                 .map(OrderLineItems::getSkuCode)
                 .toList();
-        //call inventory service and place order if product is in stock
-        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
 
-        boolean allProductInStock = Arrays.stream(inventoryResponsesArray).allMatch(InventoryResponse::isInStock);
+        log.info("Calling inventory service");
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookup.start())){
+            //call inventory service and place order if product is in stock
+            InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        if(allProductInStock){
-            orderRepository.save(order);
-            return "Order placed successfully";
-        }else {
-            throw new IllegalArgumentException("Product is not in stock");
+            boolean allProductInStock = Arrays.stream(inventoryResponsesArray).allMatch(InventoryResponse::isInStock);
+
+            if(allProductInStock){
+                orderRepository.save(order);
+                return "Order placed successfully";
+            }else {
+                throw new IllegalArgumentException("Product is not in stock");
+            }
+        }finally {
+            inventoryServiceLookup.end();
         }
 
     }
